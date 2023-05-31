22b365fd5157a2bf7a76c6e392a48c5c2e584efd
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2023-05-31 14:03:12 +0330

    repositioned player and roads and added left/right

--- Assets/playerMove.cs
+++ Assets/playerMove.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
+    public Rigidbody rb;
+    public GameObject rightLane;
+    public GameObject leftLane;
+ 
+    public float speed = 5;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        //below code could be used for restarting from the start of the road
+        Vector3 lo = GameObject.FindWithTag("rightLane").transform.position - (GameObject.FindWithTag("rightLane").transform.localScale / 2);
+        Debug.Log(lo);
+        transform.position = lo + new Vector3(transform.localScale.x/1.2f,2f,0);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        rb.AddForce(0, 0, speed);
+        if (Input.GetKeyDown("right"))
+        {
+            transform.position = new Vector3(rightLane.transform.position.x, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            transform.position = new Vector3(leftLane.transform.position.x, transform.position.y, transform.position.z);
+        }
     }
 }
