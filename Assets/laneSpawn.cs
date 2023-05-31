532e0cc85e1d4ac377655b4fd2066ec261e941b0
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2023-05-31 17:31:50 +0330

    added something

--- Assets/laneSpawn.cs
+++ Assets/laneSpawn.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class laneSpawn: MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     public int spawnRate = 2;
     public double timer = 0d;
     public GameObject lanes;
+    float currentSpeed;
     Vector3 offset;
     // Start is called before the first frame update
     // Update is called once per frame
     void Start()
     {
-        offset = transform.position - player.position;
+        offset = transform.position - player.transform.position;
         Debug.Log(offset);
     }
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z + 10f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + 10f);
+        //currentSpeed = GameObject.FindAnyObjectByType<Rigidbody>(FindObjectOfType)
         if (timer < spawnRate)
         {
             timer += Time.deltaTime;
+
             //Debug.Log(timer);
         }
         else
