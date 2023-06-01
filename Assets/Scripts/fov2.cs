03bb6eaae0966c96257774d33620db612f30252c
Amirreza Taghizadeh <amirreza@Amirrezas-MacBook-Pro.local>
2023-06-01 18:24:48 +0330

    Added Neon Material, not working as intended!

--- Assets/Scripts/fov2.cs
+++ Assets/Scripts/fov2.cs
@@ -7,7 +7,7 @@ public class fov2: MonoBehaviour
     // Start is called before the first frame update
     public Light light;
     public Transform player;
-    public float disappearRate = 10;
+    public float disappearRate = 5;
     // Update is called once per frame
     Vector3 initialPos;
     void Start()
@@ -16,9 +16,9 @@ public class fov2: MonoBehaviour
     }
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z + initialPos.z);
+        transform.position = new Vector3(player.position.x + initialPos.x, transform.position.y, player.position.z + initialPos.z);
         //transform.position = Vector3.Lerp(transform.position, player.position + initialPos, 1);
 
-        light.range -= Time.deltaTime;
+        light.range -= Time.deltaTime / disappearRate;
     }
 }
