3ab11d12d962030870f0ab2194e73f1e1d2bdfaa
Arian Bagheri <arian2000bagheri@yahoo.com>
2023-06-17 21:21:09 +0330

    ui added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,14 +9,40 @@ public class GameManager : MonoBehaviour
     [SerializeField] private FogController fogController;
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private UIManager uiManager;
-    private void Update()
+
+
+    private static GameManager instance;
+    public static GameManager Instance
     {
-        if (Input.GetMouseButtonDown(0))
+        get
         {
-            fogController.enabled = true;
-            playerMovement.enabled = true;
-            
-            uiManager.StartGame();
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    instance = singleton.AddComponent<GameManager>();
+                    singleton.name = "GameManagerSingleton";
+                    DontDestroyOnLoad(singleton);
+                }
+            }
+
+            return instance;
         }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void StartLevel()
+    {
+        fogController.enabled = true;
+        playerMovement.enabled = true;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Net.Mime;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,18 +8,56 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private TMP_Text startText;
+    [SerializeField] private TMP_Text levelNumberText;
+    [SerializeField] private GameObject startUI;
     [SerializeField] private Button resetButton;
+    [SerializeField] private RectTransform levels;
+    [SerializeField] private Button backgroundButton;
     
+    private Animator levelsSelectorAnimator;
+
+    private void Awake()
+    {
+        backgroundButton.onClick.AddListener(StartGame);
+        levelNumberText.text = "Level " + (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Start()
+    {
+        levelsSelectorAnimator = levels.GetComponent<Animator>();
+    }
+
     public void StartGame()
     {
-        startText.gameObject.SetActive(false);
+        startUI.SetActive(false);
         resetButton.gameObject.SetActive(true);
+
+        GameManager.Instance.StartLevel();
     }
-    
-    
+
+    public void OpenLevelSelector()
+    {
+        levelsSelectorAnimator.SetTrigger("Open");
+
+        backgroundButton.onClick.RemoveAllListeners();
+        backgroundButton.onClick.AddListener(CloseLevelSelector);
+    }
+
+    private void CloseLevelSelector()
+    {
+        levelsSelectorAnimator.SetTrigger("Close");
+        
+        backgroundButton.onClick.RemoveAllListeners();
+        backgroundButton.onClick.AddListener(StartGame);
+    }
+
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SelectLevel(int levelNumber)
+    {
+        SceneManager.LoadScene(levelNumber);
     }
 }
