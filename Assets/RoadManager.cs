b1052c339f167de05261f224f7dcc35d3ea6b181
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2023-08-04 13:48:27 +0330

    replaced AggregatingQueue w/ 2 separate R&LQueue

--- Assets/RoadManager.cs
+++ Assets/RoadManager.cs
@@ -5,17 +5,19 @@ using ArianWorkplace;
 
 public class RoadManager : MonoBehaviour
 {
-    Queue<GameObject> untrackedRightRoads = new Queue<GameObject>();
     Queue<GameObject> trackingRightRoads = new Queue<GameObject>();
-    Queue<GameObject> untrackedLeftRoads = new Queue<GameObject>();
     Queue<GameObject> trackingLeftRoads = new Queue<GameObject>();
     GameObject notcontactingRoad;
     GameObject contactingRoad;
-    GameObject[] lastEnquedRoads = new GameObject[2];
+    GameObject dequeuedRightRoad;
+    GameObject dequeuedLeftRoad;
+    int fixedSeconds = 6;
+    float roadGap = 0;
+    GameObject[] lastEnquedRoads = new GameObject[2]; //for placing the next enqueue with repsepct to last enqueued road
     int counter = 0;
     public GameObject player;
-    public GameObject[] leftRoads = new GameObject[10];
-    public GameObject[] rightRoads = new GameObject[10];
+    public GameObject[] leftRoads = new GameObject[6];
+    public GameObject[] rightRoads = new GameObject[6];
     ObjectPool objectPool;
     GameObject leftLane;
     PlayerMovement playerMovement;
@@ -34,20 +36,21 @@ public class RoadManager : MonoBehaviour
         trackingLeftRoads.Enqueue(leftRoads[0]);
         trackingRightRoads.Enqueue(rightRoads[0]);
 
-        for (int i = 1; i < 10; i++)
+        for (int i = 1; i < 6; i++)
         {
             rightRoads[i] = Instantiate(GameObject.FindGameObjectWithTag("RoadC"));
             leftRoads[i] = Instantiate(GameObject.FindGameObjectWithTag("RoadC"));
-            rightRoads[i].transform.position = rightRoads[i - 1].transform.position + new Vector3(0, 0, 6.7f);
-            leftRoads[i].transform.position = leftRoads[i - 1].transform.position + new Vector3(0, 0, 6.7f);
+            rightRoads[i].transform.position = rightRoads[i - 1].transform.position + new Vector3(0, 0, 6.69f);
+            leftRoads[i].transform.position = leftRoads[i - 1].transform.position + new Vector3(0, 0, 6.69f);
             trackingLeftRoads.Enqueue(leftRoads[i]);
             trackingRightRoads.Enqueue(rightRoads[i]);
-            if(i == 9) {
+            if(i == 5) {
                 lastEnquedRoads[0] = rightRoads[i];
                 lastEnquedRoads[1] = leftRoads[i];
             }
         }
-
+        dequeuedRightRoad = trackingRightRoads.Dequeue();
+        dequeuedLeftRoad = trackingLeftRoads.Dequeue();
         player = GameObject.FindGameObjectWithTag("Player");
     }
     
@@ -59,9 +62,11 @@ public class RoadManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (playerMovement.speed > 5) { fixedSeconds = 3; }
         if (counter == 0)
         {
-            StartCoroutine(UpdateQueues());
+            StartCoroutine(UpdateRightQueue());
+            StartCoroutine(UpdateLeftQueue());
             counter++;
         }
         //timer += Time.deltaTime;
@@ -69,46 +74,78 @@ public class RoadManager : MonoBehaviour
         //testTime += Time.deltaTime;
         //Debug.Log(Time.deltaTime);
     }
-    IEnumerator UpdateQueues()
+    IEnumerator UpdateRightQueue()
     {
         while (true)
         {
             //Debug.Log("hi");
-            if(player.transform.position.x == -0.6f) {
-                contactingRoad = trackingLeftRoads.Dequeue();
-                notcontactingRoad = trackingRightRoads.Dequeue();
-            }
-            else {
-                contactingRoad = trackingRightRoads.Dequeue();
-                notcontactingRoad = trackingLeftRoads.Dequeue();
-            }
+            
+            //notcontactingRoad = trackingRightRoads.Dequeue();
+            //else {
+            //    contactingRoad = trackingRightRoads.Dequeue();
+            //    notcontactingRoad = trackingLeftRoads.Dequeue();
+            //}
             
             //Debug.Log(contactingRoad.transform.position.z);
 
-            while (player.transform.position.z <= contactingRoad.transform.position.z + 3.65f && player.transform.position.x >= contactingRoad.transform.position.x - 0.2f )
+            while (player.transform.position.z <= dequeuedRightRoad.transform.position.z + 3.65f) //player.transform.position.x >= contactingRoad.transform.position.x - 0.2f
             {
-                Debug.Log("you are in this Lane:" + contactingRoad.transform.position.z);
+
+                //Debug.Log("The field of view contains this road: " + dequeuedRightRoad.transform.position.z);
                 yield return null;
             }
-            Debug.Log("hellooooo000000000000000!");
-            yield return new WaitForSeconds(6/playerMovement.speed);
-            if (contactingRoad.transform.position.x == 0.6f) {
-                contactingRoad.transform.position = lastEnquedRoads[0].transform.position + new Vector3(0, 0, 6.7f);
-                notcontactingRoad.transform.position = lastEnquedRoads[1].transform.position + new Vector3(0, 0, 6.7f);
+            yield return new WaitForSeconds(fixedSeconds/playerMovement.speed);
+            //Debug.Log("We've exited the right road: Enqueue process of this road and the next dequeue begins!");
+            dequeuedRightRoad.transform.position = lastEnquedRoads[0].transform.position + new Vector3(0, 0, 6.69f);
+            lastEnquedRoads[0] = dequeuedRightRoad;
+            trackingRightRoads.Enqueue(lastEnquedRoads[0]);
+            dequeuedRightRoad = trackingRightRoads.Dequeue();
 
-                lastEnquedRoads[0] = contactingRoad;
-                trackingRightRoads.Enqueue(lastEnquedRoads[0]);
-                lastEnquedRoads[1] = notcontactingRoad;
-                trackingLeftRoads.Enqueue(lastEnquedRoads[1]);
+            //else {
+            //    contactingRoad.transform.position = lastEnquedRoads[1].transform.position + new Vector3(0, 0, 69f);
+            //    notcontactingRoad.transform.position = lastEnquedRoads[0].transform.position + new Vector3(0, 0, 6.69f);
+            //    lastEnquedRoads[1] = contactingRoad;
+            //    trackingLeftRoads.Enqueue(lastEnquedRoads[1]);
+            //    lastEnquedRoads[0] = notcontactingRoad;
+            //    trackingRightRoads.Enqueue(lastEnquedRoads[0]);
+            //}
+        }
+    }
+    IEnumerator UpdateLeftQueue()
+    {
+        while (true)
+        {
+            //Debug.Log("hi");
+
+            //notcontactingRoad = trackingRightRoads.Dequeue();
+            //else {
+            //    contactingRoad = trackingRightRoads.Dequeue();
+            //    notcontactingRoad = trackingLeftRoads.Dequeue();
+            //}
+
+            //Debug.Log(contactingRoad.transform.position.z);
+
+            while (player.transform.position.z <= dequeuedLeftRoad.transform.position.z + 3.65f) //player.transform.position.x >= contactingRoad.transform.position.x - 0.2f
+            {
+
+                Debug.Log("The field of view contains this road: " + dequeuedLeftRoad.transform.position.z);
+                yield return null;
             }
-            else {
-                contactingRoad.transform.position = lastEnquedRoads[1].transform.position + new Vector3(0, 0, 6.7f);
-                notcontactingRoad.transform.position = lastEnquedRoads[0].transform.position + new Vector3(0, 0, 6.7f);
-                lastEnquedRoads[1] = contactingRoad;
-                trackingLeftRoads.Enqueue(lastEnquedRoads[1]);
-                lastEnquedRoads[0] = notcontactingRoad;
-                trackingRightRoads.Enqueue(lastEnquedRoads[0]);
-            }
+            yield return new WaitForSeconds(fixedSeconds / playerMovement.speed);
+            Debug.Log("We've exited the left road: Enqueue process of this road and the next dequeue begins!");
+            dequeuedLeftRoad.transform.position = lastEnquedRoads[1].transform.position + new Vector3(0, 0, 6.69f);
+            lastEnquedRoads[1] = dequeuedLeftRoad;
+            trackingLeftRoads.Enqueue(lastEnquedRoads[1]);
+            dequeuedLeftRoad = trackingLeftRoads.Dequeue();
+
+            //else {
+            //    contactingRoad.transform.position = lastEnquedRoads[1].transform.position + new Vector3(0, 0, 69f);
+            //    notcontactingRoad.transform.position = lastEnquedRoads[0].transform.position + new Vector3(0, 0, 6.69f);
+            //    lastEnquedRoads[1] = contactingRoad;
+            //    trackingLeftRoads.Enqueue(lastEnquedRoads[1]);
+            //    lastEnquedRoads[0] = notcontactingRoad;
+            //    trackingRightRoads.Enqueue(lastEnquedRoads[0]);
+            //}
         }
     }
     ////////////////////////
